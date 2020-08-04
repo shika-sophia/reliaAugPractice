71eafb6c9ac337699232a88d136eeb45e308276e
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2020-08-04 12:37:30 +0900

    2020-08-04
    chapter1 / List 1-1, 1-2, 1-3

---
+++ chapter1/Aisatsu.java
@@ -0,0 +1,33 @@
+/**
+ * @title chapter1 / List 1-2, 1-3
+ * @author Oshika
+ * @date 2020-08-04
+ */
+package chapter1;
+
+public class Aisatsu {
+
+    public static void main(String[] args) {
+        System.out.println("おはよう。");
+        System.out.println("こんにちは。");
+        System.out.println("こんばんは。");
+
+    }//maim()
+
+}//class
+
+/* //==== Result ====
+おはよう。
+こんにちは。
+こんばんは。
+*/
+
+//==== List 1-3 ====
+/*間違い探し
+1.public アクセス修飾子、すべてのクラスから参照可能
+2.static 静的メソッド（クラスメンバー）の宣言、既定でインスタンスされる
+3.「"」の閉じ忘れに注意
+4.終了符「;」、ラベル「:」
+5.クラス・メッソドの階層は「.」で区切る
+6.ブロックの閉じ忘れ、開始位置と同じインデントで
+ */
---
+++ chapter1/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * @title chapter1/ List 1-1
+ * @author Oshika
+ * @date 2020-08-04
+ */
+package chapter1;
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }//main()
+
+}//class
+
+//==== Result ====
+//Hello!
