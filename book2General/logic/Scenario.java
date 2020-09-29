7865ad1759a3865bb486824f974fa050cf3e2bc2
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2020-09-29 17:10:40 +0900

    2020-09-29-1700

--- book2General/logic/Scenario.java
+++ book2General/logic/Scenario.java
@@ -18,11 +18,15 @@ package book2General.logic;
 import java.util.ArrayList;
 import java.util.List;
 
+import book2General.corpsRevision.DataFileReader;
+import book2General.corpsRevision.Division;
+
 public class Scenario {
     private static int sceneCount = 1;
     private int sceneNum;
     private String sceneStr;
     private List<String> sceneGeneral;
+    private List<Division> sceneDivList;
 
     public Scenario() {
         this.sceneNum = sceneCount;
@@ -30,24 +34,24 @@ public class Scenario {
         sceneCount++;
     }
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//        //繰り返しインスタンス・表示テスト
+//        for(int i = 1; i <= 7; i++) {
+//            //コンストラクター呼び出し
+//            Scenario scenario =new Scenario();
+//            scenario.takeScene();
+//
+//            System.out.println("sceneNum: " + scenario.sceneNum);
+//            System.out.println("sceneStr: " + scenario.sceneStr);
+//            System.out.println("sceneGeneral: " + scenario.sceneGeneral);
+//            System.out.println();
+//        }//for scene
+//
+//    }//main()
 
-        //繰り返しインスタンス・表示テスト
-        for(int i = 1; i <= 7; i++) {
-            //コンストラクター呼び出し
-            Scenario scenario =new Scenario();
-            scenario.takeScene();
 
-            System.out.println("sceneNum: " + scenario.sceneNum);
-            System.out.println("sceneStr: " + scenario.sceneStr);
-            System.out.println("sceneGeneral: " + scenario.sceneGeneral);
-            System.out.println();
-        }//for scene
-
-    }//main()
-
-
-    public void takeScene() {
+    public void takeScene(DataFileReader data) {
 
         List<String> sceneGeneral = new ArrayList<>();
 
@@ -57,6 +61,8 @@ public class Scenario {
             sceneGeneral.add("宮部継潤");
             sceneGeneral.add("阿閉貞秀");
             sceneGeneral.add("佐久間信盛");
+            sceneGeneral.add("朝倉景鏡");
+            sceneGeneral.add("酒井忠次");
             break;
 
         case 2:
@@ -64,6 +70,9 @@ public class Scenario {
             sceneGeneral.add("宮部継潤");
             sceneGeneral.add("阿閉貞秀");
             sceneGeneral.add("池田恒興");
+            sceneGeneral.add("前波吉継");
+            sceneGeneral.add("酒井忠次");
+            sceneGeneral.add("本多忠勝");
             break;
 
         case 3:
@@ -102,8 +111,43 @@ public class Scenario {
         //setSceneStr(sceneStr);//直接フィールドに代入してるので不要
         setSceneGeneral(sceneGeneral);
 
+        //登場武将のList<String> を検索して List<Division>にしてフィールドに保存
+        this.sceneDivList = searchDivision(data, sceneGeneral);
+
     }//takeScene()
 
+
+    //登場武将のList<String> を検索して List<Division>にしておく
+    public List<Division> searchDivision(DataFileReader data, List<String> sceneGeneral) {
+        //登場武将のList<Division> これから入れる器
+        List<Division> sceneDivList = new ArrayList<>();
+
+        //登場武将を１つずつ取り出して、全Divisionから検索
+        List<Division> divisionList = data.getDivisionList();
+
+        for (String generalBit : sceneGeneral) {
+            for (Division div : divisionList) {
+                //登場武将が Divisionの名前と一致したら、
+                //sceneDivListに登録し検索終了して、次の検索へ
+                if (generalBit.equals(div.getName())){
+                    sceneDivList.add(div);
+                    break;
+                }//if
+            }//div
+        }//for generalBit
+
+        return sceneDivList;
+    }//searchDivision()
+
+
+    //====== getter , setter ======
+    public List<Division> getSceneDivList() {
+        return sceneDivList;
+    }
+
+    public void setSceneDivList(List<Division> sceneDivList) {
+        this.sceneDivList = sceneDivList;
+    }
 
     public String getSceneStr() {
         return sceneStr;
